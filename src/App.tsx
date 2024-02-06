f07cc4238f4f0a28c0d08836d7943ff2721afa0a
Harshita B <honeyblue2604@gmail.com>
2024-02-05 16:42:32 -0800

    Finished card design

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
+import Card from './components/Card'
 
 function App() {
 
   return (
     <>
+      <Card 
+        title='Card title'
+        artist='Artist'
+        img='https://m.media-amazon.com/images/I/71QgmF3cnEL._UF1000,1000_QL80_.jpg'/>
     </>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,1 +1,17 @@
 import "./Card.css"
+
+const Card = (props: {img: string, title: string, artist: string}) => {
+    return (
+        <div className="card-body">
+            <img src={props.img} alt="" className="card-img"/>
+            
+            {/* Info for the card */}
+            <div className="card-info">
+                <p className="card-title">{props.title}</p>
+                <p className="card-artist">{props.artist}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
