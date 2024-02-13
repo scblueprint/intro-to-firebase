b4c92d8e5f2fecae246a828170f07c0c513c5103
Harshita B <honeyblue2604@gmail.com>
2024-02-12 17:04:41 -0800

    form styling

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -2,26 +2,18 @@ import "./Form.css"
 
 const Form = () => {
     return (
-        <>
         <div className="form-body">
-            <label htmlFor="song-name">Song Name</label>
-            <input type="text" id="song-name"/>
+            <input type="text" className="question" placeholder="Song Name"/>
+        
+            <input type="text" className="question" placeholder="Artist"/>
+        
+            <input type="text" className="question" placeholder="Album"/>
+        
+            <input type="text" className="question" placeholder="Album Cover (Image URL)"/>
             
-            <label htmlFor="song-artist">Artist</label>
-            <input type="text" id="song-artist"/>
-            
-            <label htmlFor="song-album">Album</label>
-            <input type="text" id="song-album"/>
-            
-            <label htmlFor="song-album-img">Album Cover</label>
-            <input type="text" id="song-album-img"/>
-            
-            <label htmlFor="song-album-img-prev">Album Cover Preview</label>
-            <div className="img-preview">
+            <div className="img-preview"> </div>
 
-            </div>
         </div>
-        </>
     );
 };
 
